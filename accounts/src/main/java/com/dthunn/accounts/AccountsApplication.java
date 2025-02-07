116ce8b0ed5a4e99be617dd3232cb4620ad298a3
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-02-07 12:27:43 -0700

    added feign

--- accounts/src/main/java/com/dthunn/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/dthunn/accounts/AccountsApplication.java
@@ -4,9 +4,11 @@ import com.dthunn.accounts.dto.AccountsContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableFeignClients
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
--- loans/src/main/java/com/dthunn/loans/LoansApplication.java
+++ loans/src/main/java/com/dthunn/loans/LoansApplication.java
@@ -4,8 +4,10 @@ import com.dthunn.loans.dto.LoansContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableFeignClients
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {LoansContactInfoDto.class})
