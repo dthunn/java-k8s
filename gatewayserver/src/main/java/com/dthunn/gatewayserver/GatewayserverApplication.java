b180dea716e4e09a404b91caaa45e50083824c12
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-02-08 14:45:25 -0700

    redisratelimiter added

--- gatewayserver/src/main/java/com/dthunn/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/dthunn/gatewayserver/GatewayserverApplication.java
@@ -38,27 +38,27 @@ public class GatewayserverApplication {
                 .route(p -> p
                         .path("/app/loans/**")
                         .filters(f -> f.rewritePath("/app/loans/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-//                                .retry(retryConfig -> retryConfig.setRetries(3)
-//                                        .setMethods(HttpMethod.GET)
-//                                        .setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true)))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .retry(retryConfig -> retryConfig.setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true)))
                         .uri("lb://LOANS"))
                 .route(p -> p
                         .path("/app/cards/**")
                         .filters(f -> f.rewritePath("/app/cards/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-//                                .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
-//                                        .setKeyResolver(userKeyResolver())))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(userKeyResolver())))
                         .uri("lb://CARDS")).build();
 
     }
 
-//    @Bean
-//    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
-//        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
-//                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-//                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
-//    }
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
+    }
 
     @Bean
     public RedisRateLimiter redisRateLimiter() {
